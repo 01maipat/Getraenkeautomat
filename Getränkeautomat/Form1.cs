90bf30bd09ae6c066446fc1aeb0e52c619066463
01maipat <01maipat@rgtfo-me.it>
2018-03-19 09:03:31 +0100

    Grundstruktur
    Signed-off-by: 01maipat <01maipat@rgtfo-me.it>

--- Getränkeautomat/Form1.cs
+++ Getränkeautomat/Form1.cs
@@ -11,6 +11,7 @@ namespace Getränkeautomat
 {
     public partial class Form1 : Form
     {
+        double preis;
         public Form1()
         {
             InitializeComponent();
@@ -18,48 +19,34 @@ namespace Getränkeautomat
 
         private void pb_cola_Click(object sender, EventArgs e)
         {
-            
+            preis = 1.20;
         }
 
         private void pb_fanta_Click(object sender, EventArgs e)
         {
-
+            preis = 1.50;
         }
 
         private void pb_icetea_Click(object sender, EventArgs e)
         {
-
+            preis = 1.80;
         }
 
         private void pb_spezi_Click(object sender, EventArgs e)
         {
-
+            preis = 1.00;
         }
 
         private void pb_sprite_Click(object sender, EventArgs e)
         {
-
+            preis = 1.60;
         }
 
         private void pb_water_Click(object sender, EventArgs e)
         {
-
+            preis = 0.80;
         }
 
-        private void lbl_icetea_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl_fanta_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl_cola_Click(object sender, EventArgs e)
-        {
-
-        }
 
     }
 }

12eeea2bad3767b1b7fe3d1cfc24765c1f129270
01maipat <01maipat@rgtfo-me.it>
2018-03-19 08:57:05 +0100

    Signed-off-by: 01maipat <01maipat@rgtfo-me.it>

--- Getränkeautomat/Form1.Designer.cs
+++ Getränkeautomat/Form1.Designer.cs
@@ -28,19 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbl_Preis = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.pb_cola = new System.Windows.Forms.PictureBox();
+            this.pb_fanta = new System.Windows.Forms.PictureBox();
+            this.pb_icetea = new System.Windows.Forms.PictureBox();
+            this.pb_spezi = new System.Windows.Forms.PictureBox();
+            this.pb_sprite = new System.Windows.Forms.PictureBox();
+            this.pb_water = new System.Windows.Forms.PictureBox();
+            this.lbl_fanta = new System.Windows.Forms.Label();
+            this.lbl_icetea = new System.Windows.Forms.Label();
+            this.lal_fanta = new System.Windows.Forms.Label();
+            this.lbl_sprite = new System.Windows.Forms.Label();
+            this.lbl_water = new System.Windows.Forms.Label();
+            this.lbl_cola = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cola)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_fanta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_icetea)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_spezi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_sprite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_water)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_Preis
@@ -51,74 +58,155 @@
             this.lbl_Preis.Size = new System.Drawing.Size(131, 54);
             this.lbl_Preis.TabIndex = 0;
             // 
-            // pictureBox1
+            // pb_cola
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 107);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pb_cola.Image = ((System.Drawing.Image)(resources.GetObject("pb_cola.Image")));
+            this.pb_cola.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_cola.InitialImage")));
+            this.pb_cola.Location = new System.Drawing.Point(12, 107);
+            this.pb_cola.Name = "pb_cola";
+            this.pb_cola.Size = new System.Drawing.Size(100, 50);
+            this.pb_cola.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_cola.TabIndex = 1;
+            this.pb_cola.TabStop = false;
+            this.pb_cola.Click += new System.EventHandler(this.pb_cola_Click);
             // 
-            // pictureBox2
+            // pb_fanta
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(12, 163);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pb_fanta.Image = ((System.Drawing.Image)(resources.GetObject("pb_fanta.Image")));
+            this.pb_fanta.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_fanta.InitialImage")));
+            this.pb_fanta.Location = new System.Drawing.Point(12, 163);
+            this.pb_fanta.Name = "pb_fanta";
+            this.pb_fanta.Size = new System.Drawing.Size(100, 50);
+            this.pb_fanta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_fanta.TabIndex = 2;
+            this.pb_fanta.TabStop = false;
+            this.pb_fanta.Click += new System.EventHandler(this.pb_fanta_Click);
             // 
-            // pictureBox3
+            // pb_icetea
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(12, 219);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.pb_icetea.Image = ((System.Drawing.Image)(resources.GetObject("pb_icetea.Image")));
+            this.pb_icetea.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_icetea.InitialImage")));
+            this.pb_icetea.Location = new System.Drawing.Point(12, 219);
+            this.pb_icetea.Name = "pb_icetea";
+            this.pb_icetea.Size = new System.Drawing.Size(100, 50);
+            this.pb_icetea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_icetea.TabIndex = 3;
+            this.pb_icetea.TabStop = false;
+            this.pb_icetea.Click += new System.EventHandler(this.pb_icetea_Click);
             // 
-            // pictureBox4
+            // pb_spezi
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(12, 275);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox4.TabIndex = 4;
-            this.pictureBox4.TabStop = false;
+            this.pb_spezi.Image = ((System.Drawing.Image)(resources.GetObject("pb_spezi.Image")));
+            this.pb_spezi.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_spezi.InitialImage")));
+            this.pb_spezi.Location = new System.Drawing.Point(12, 275);
+            this.pb_spezi.Name = "pb_spezi";
+            this.pb_spezi.Size = new System.Drawing.Size(100, 50);
+            this.pb_spezi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_spezi.TabIndex = 4;
+            this.pb_spezi.TabStop = false;
+            this.pb_spezi.Click += new System.EventHandler(this.pb_spezi_Click);
             // 
-            // pictureBox5
+            // pb_sprite
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(12, 331);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox5.TabIndex = 5;
-            this.pictureBox5.TabStop = false;
+            this.pb_sprite.Image = ((System.Drawing.Image)(resources.GetObject("pb_sprite.Image")));
+            this.pb_sprite.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_sprite.InitialImage")));
+            this.pb_sprite.Location = new System.Drawing.Point(12, 331);
+            this.pb_sprite.Name = "pb_sprite";
+            this.pb_sprite.Size = new System.Drawing.Size(100, 50);
+            this.pb_sprite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_sprite.TabIndex = 5;
+            this.pb_sprite.TabStop = false;
+            this.pb_sprite.Click += new System.EventHandler(this.pb_sprite_Click);
             // 
-            // pictureBox6
+            // pb_water
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(12, 387);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox6.TabIndex = 6;
-            this.pictureBox6.TabStop = false;
+            this.pb_water.Image = ((System.Drawing.Image)(resources.GetObject("pb_water.Image")));
+            this.pb_water.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb_water.InitialImage")));
+            this.pb_water.Location = new System.Drawing.Point(12, 387);
+            this.pb_water.Name = "pb_water";
+            this.pb_water.Size = new System.Drawing.Size(100, 50);
+            this.pb_water.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_water.TabIndex = 6;
+            this.pb_water.TabStop = false;
+            this.pb_water.Click += new System.EventHandler(this.pb_water_Click);
+            // 
+            // lbl_fanta
+            // 
+            this.lbl_fanta.BackColor = System.Drawing.Color.Lime;
+            this.lbl_fanta.Location = new System.Drawing.Point(118, 175);
+            this.lbl_fanta.Name = "lbl_fanta";
+            this.lbl_fanta.Size = new System.Drawing.Size(35, 28);
+            this.lbl_fanta.TabIndex = 8;
+            this.lbl_fanta.Click += new System.EventHandler(this.lbl_fanta_Click);
+            // 
+            // lbl_icetea
+            // 
+            this.lbl_icetea.BackColor = System.Drawing.Color.Lime;
+            this.lbl_icetea.Location = new System.Drawing.Point(118, 232);
+            this.lbl_icetea.Name = "lbl_icetea";
+            this.lbl_icetea.Size = new System.Drawing.Size(35, 27);
+            this.lbl_icetea.TabIndex = 9;
+            this.lbl_icetea.Click += new System.EventHandler(this.lbl_icetea_Click);
+            // 
+            // lal_fanta
+            // 
+            this.lal_fanta.BackColor = System.Drawing.Color.Lime;
+            this.lal_fanta.Location = new System.Drawing.Point(118, 288);
+            this.lal_fanta.Name = "lal_fanta";
+            this.lal_fanta.Size = new System.Drawing.Size(35, 27);
+            this.lal_fanta.TabIndex = 10;
+            // 
+            // lbl_sprite
+            // 
+            this.lbl_sprite.BackColor = System.Drawing.Color.Lime;
+            this.lbl_sprite.Location = new System.Drawing.Point(118, 341);
+            this.lbl_sprite.Name = "lbl_sprite";
+            this.lbl_sprite.Size = new System.Drawing.Size(35, 31);
+            this.lbl_sprite.TabIndex = 11;
+            // 
+            // lbl_water
+            // 
+            this.lbl_water.BackColor = System.Drawing.Color.Lime;
+            this.lbl_water.Location = new System.Drawing.Point(118, 398);
+            this.lbl_water.Name = "lbl_water";
+            this.lbl_water.Size = new System.Drawing.Size(35, 30);
+            this.lbl_water.TabIndex = 12;
+            // 
+            // lbl_cola
+            // 
+            this.lbl_cola.BackColor = System.Drawing.Color.Lime;
+            this.lbl_cola.Location = new System.Drawing.Point(118, 119);
+            this.lbl_cola.Name = "lbl_cola";
+            this.lbl_cola.Size = new System.Drawing.Size(35, 29);
+            this.lbl_cola.TabIndex = 13;
+            this.lbl_cola.Click += new System.EventHandler(this.lbl_cola_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(321, 522);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_cola);
+            this.Controls.Add(this.lbl_water);
+            this.Controls.Add(this.lbl_sprite);
+            this.Controls.Add(this.lal_fanta);
+            this.Controls.Add(this.lbl_icetea);
+            this.Controls.Add(this.lbl_fanta);
+            this.Controls.Add(this.pb_water);
+            this.Controls.Add(this.pb_sprite);
+            this.Controls.Add(this.pb_spezi);
+            this.Controls.Add(this.pb_icetea);
+            this.Controls.Add(this.pb_fanta);
+            this.Controls.Add(this.pb_cola);
             this.Controls.Add(this.lbl_Preis);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_cola)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_fanta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_icetea)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_spezi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_sprite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_water)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -126,12 +214,18 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_Preis;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pb_cola;
+        private System.Windows.Forms.PictureBox pb_fanta;
+        private System.Windows.Forms.PictureBox pb_icetea;
+        private System.Windows.Forms.PictureBox pb_spezi;
+        private System.Windows.Forms.PictureBox pb_sprite;
+        private System.Windows.Forms.PictureBox pb_water;
+        private System.Windows.Forms.Label lbl_fanta;
+        private System.Windows.Forms.Label lbl_icetea;
+        private System.Windows.Forms.Label lal_fanta;
+        private System.Windows.Forms.Label lbl_sprite;
+        private System.Windows.Forms.Label lbl_water;
+        private System.Windows.Forms.Label lbl_cola;
     }
 }
 
--- Getränkeautomat/Form1.cs
+++ Getränkeautomat/Form1.cs
@@ -15,5 +15,51 @@ namespace Getränkeautomat
         {
             InitializeComponent();
         }
+
+        private void pb_cola_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pb_fanta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_icetea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_spezi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_sprite_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pb_water_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_icetea_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_fanta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_cola_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
